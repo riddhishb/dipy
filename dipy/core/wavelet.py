16668e415b01579b3b3aa9087a0c2a83e2ad3f6e
Riddhish Bhalodia <Riddhish@Riddhishs-MacBook-Pro.local>
2016-03-19 13:02:23 +0530

    added wavelet docsrings

--- dipy/core/wavelet.py
+++ dipy/core/wavelet.py
@@ -1,8 +1,31 @@
 import numpy as np
 from dipy.denoise import ornlm
 
+'''
+ Functions for Wavelet Transforms in 3D domain
+
+ Code adapted from
+
+ WAVELET SOFTWARE AT POLYTECHNIC UNIVERSITY, BROOKLYN, NY
+ http://taco.poly.edu/WaveletSoftware/
+'''
+
 
 def cshift3D(x, m, d):
+    '''
+    3D Circular Shift
+    
+    USAGE:
+       y = cshift3D(x, m, d)
+    INPUT:
+       x - N1 by N2 by N3 array
+       m - amount of shift
+       d - dimension of shift (d = 1,2,3)
+    OUTPUT:
+       y - array x will be shifed by m samples down
+           along dimension d
+
+    '''
     s = x.shape
     idx = (np.array(range(s[d])) + (s[d] - m % s[d])) % s[d]
     if d == 0:
@@ -14,6 +37,10 @@ def cshift3D(x, m, d):
 
 
 def permutationInverse(perm):
+    '''
+    Function generating inverse of the permutation
+    '''
+
     inverse = [0] * len(perm)
     for i, p in enumerate(perm):
         inverse[p] = i
@@ -21,6 +48,23 @@ def permutationInverse(perm):
 
 
 def afb3D_A(x, af, d):
+    
+    '''
+    3D Analysis Filter Bank
+     (along one dimension only)
+    
+     [lo, hi] = afb3D_A(x, af, d);
+    INPUT:
+        x - N1xN2xN2 matrix, where min(N1,N2,N3) > 2*length(filter)
+               (Ni are even)
+        af - analysis filter for the columns
+        af(:, 1) - lowpass filter
+        af(:, 2) - highpass filter
+        d - dimension of filtering (d = 1, 2 or 3)
+    OUTPUT:
+         lo, hi - lowpass, highpass subbands
+    '''
+
     lpf = af[:, 0]
     hpf = af[:, 1]
     # permute dimensions of x so that dimension d is first.
@@ -50,6 +94,15 @@ def afb3D_A(x, af, d):
 
 
 def sfb3D_A(lo, hi, sf, d):
+    '''
+    3D Synthesis Filter Bank
+     (along single dimension only)
+    
+     y = sfb3D_A(lo, hi, sf, d);
+     sf - synthesis filters
+     d  - dimension of filtering
+
+    '''
     lpf = sf[:, 0]
     hpf = sf[:, 1]
     # permute dimensions of lo and hi so that dimension d is first.
@@ -74,6 +127,18 @@ def sfb3D_A(lo, hi, sf, d):
 
 
 def sfb3D(lo, hi, sf1, sf2=None, sf3=None):
+    '''
+    3D Synthesis Filter Bank
+    
+    USAGE:
+       y = sfb3D(lo, hi, sf1, sf2, sf3);
+    INPUT:
+       lo, hi - lowpass subbands
+       sfi - synthesis filters for dimension i
+    OUPUT:
+       y - output array
+
+    '''
     if sf2 is None:
         sf2 = sf1
     if sf3 is None:
@@ -100,6 +165,24 @@ def sfb3D(lo, hi, sf1, sf2=None, sf3=None):
 
 
 def afb3D(x, af1, af2=None, af3=None):
+    '''
+    3D Analysis Filter Bank
+    USAGE:
+        [lo, hi] = afb3D(x, af1, af2, af3);
+    INPUT:
+        x - N1 by N2 by N3 array matrix, where
+            1) N1, N2, N3 all even
+            2) N1 >= 2*length(af1)
+            3) N2 >= 2*length(af2)
+            4) N3 >= 2*length(af3)
+        afi - analysis filters for dimension i
+           afi(:, 1) - lowpass filter
+           afi(:, 2) - highpass filter
+    OUTPUT:
+        lo - lowpass subband
+        hi[d], d = 1..7 - highpass subbands
+    '''
+
     if af2 is None:
         af2 = af1
     if af3 is None:
@@ -118,6 +201,22 @@ def afb3D(x, af1, af2=None, af3=None):
 
 
 def dwt3D(x, J, af):
+    '''
+    3-D Discrete Wavelet Transform
+    
+    USAGE:
+       w = dwt3D(x, stages, af)
+    INPUT:
+       x - N1 by N2 by N3 matrix
+           1) Ni all even
+           2) min(Ni) >= 2^(J-1)*length(af)
+       J - number of stages
+       af  - analysis filters
+    OUTPUT:
+       w - cell array of wavelet coefficients
+
+    '''
+
     w = [None] * (J + 1)
     for k in xrange(J):
         x, w[k] = afb3D(x, af, af, af)
@@ -126,6 +225,20 @@ def dwt3D(x, J, af):
 
 
 def idwt3D(w, J, sf):
+    '''
+    Inverse 3-D Discrete Wavelet Transform
+    
+    USAGE:
+       y = idwt3D(w, J, sf)
+    INPUT:
+       w - wavelet coefficient
+       J  - number of stages
+       sf - synthesis filters
+    OUTPUT:
+       y - output array
+
+    '''
+
     y = w[J]
     for k in range(J)[::-1]:
         y = sfb3D(y, w[k], sf, sf, sf)
